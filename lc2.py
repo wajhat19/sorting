1df6b754c59558e9fe1ca92167201373f1bd5e58
wajhat19 <wajhatpathan9@gmail.com>
2024-12-30 19:50:02 +0530

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -1,2 +1,6 @@
-val=[1,2,3,4]
-print([f'even value{x}' if x%2==0 else f'odd value{x}' for x in  [1,2,3,4]])
+#val=[1,2,3,4]
+#print([f'even value{x}' if x%2==0 else f'odd value{x}' for x in  [1,2,3,4]])
+x=3
+y=x
+x=10
+print(y)
